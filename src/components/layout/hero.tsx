9a694c8613fda3f9b7ba2c94898bb9a5ae3fbc73
harrisazmi <azmi.harris@gmail.com>
2025-08-14 21:36:50 +0800

    chore:contact page, fix theme and shadow

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -13,18 +13,13 @@ export default function Hero({
 }) {
   return (
     <section className="relative border-b border-outline-200">
-      <div className="absolute -z-10 flex h-full w-full justify-center overflow-hidden">
-        <HeroPattern className="absolute -top-[83.33%]" />
+      <div className="absolute -z-10 flex h-full w-full justify-center overflow-x-hidden">
+        <HeroPattern className="absolute" />
       </div>
 
-      <div className="container flex flex-col gap-6 py-16">
-        <h1
-          className="text-center font-poppins text-[2rem]/10 font-semibold sm:text-hmd"
-          {...(splaskPrivacyPolicy && {
-            "splwpk-privacy-policy": "splwpk-privacy-policy",
-          })}
-        >
-          {title}
+      <div className=" flex flex-col gap-6 py-16">
+        <h1 className="text-center font-poppins font-semibold">
+          <div className="text-[2rem]/10"> {title}</div>
         </h1>
         {search}
       </div>
