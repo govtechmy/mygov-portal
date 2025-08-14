592cb3bba3c1c800221253504068d9feef42f29c
harrisazmi <azmi.harris@gmail.com>
2025-08-14 22:58:01 +0800

    chore : Add basic Auth

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const PASSWORD = process.env.AUTH_TOKEN;
+const USER = process.env.AUTH_USER;
+
+export function middleware(req: NextRequest) {
+  const authHeader = req.headers.get("authorization");
+
+  if (authHeader) {
+    const [scheme, encoded] = authHeader.split(" ");
+
+    if (scheme === "Basic") {
+      const buff = Buffer.from(encoded, "base64");
+      const [user, pass] = buff.toString().split(":");
+
+      if (user === "admin" && pass === PASSWORD) {
+        return NextResponse.next();
+      }
+    }
+  }
+
+  return new NextResponse("Authentication required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+// Run on all paths
+export const config = {
+  matcher: ["/:path*"],
+};
