20fc390ff2a5583f455e609d794e7902c5d82150
PyConqueror <wanaqim@gmail.com>
2025-08-15 16:53:44 +0800

    hotfix : solve import error

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -135,18 +135,14 @@ export default function HomePage() {
           <div className="flex-[400px] lg:pt-16 lg:px-16 pt-8 px-8">
             <div className="mb-4 text-4xl lg:text-2xl md:text-xl sm:text-lg">
               Apa itu MyGOV Malaysia?
-      <section className="p-8 lg:p-16 md:p-12 sm:p-4 max-w-[1000px] mx-auto">
-        <div className="flex flex-row flex-wrap items-stretch justify-center rounded-[32px] border border-[#F4F4F5]">
-          <div className="flex-[400px] lg:pt-16 lg:px-16 pt-8 px-8">
-            <div className="mb-4 text-4xl lg:text-2xl md:text-xl sm:text-lg">
-              Apa itu MyGOV Malaysia?
             </div>
 
             <div className="text-base leading-relaxed">
-              MyGOV Malaysia merupakan aplikasi yang dibangunkan sebagai pusat rujukan
-              sehenti bagi perkhidmatan umum yang ditawarkan kerajaan Malaysia.
-              Melalui MyGOV Malaysia, anda dapat melakukan semakan, membuat permohonan,
-              menerima notifikasi di samping ciri-ciri lain yang bakal dilancarkan.
+              MyGOV Malaysia merupakan aplikasi yang dibangunkan sebagai pusat
+              rujukan sehenti bagi perkhidmatan umum yang ditawarkan kerajaan
+              Malaysia. Melalui MyGOV Malaysia, anda dapat melakukan semakan,
+              membuat permohonan, menerima notifikasi di samping ciri-ciri lain
+              yang bakal dilancarkan.
               <div className="pb-10"></div>
             </div>
           </div>
