5de752f83b04f395a561e58ed92cd90aa46cb896
PyConqueror <wanaqim@gmail.com>
2025-08-15 22:16:25 +0800

    feat(home): centralize homepage constants and pass via server page
    - Add constants aggregator at src/constants/home/index.ts exporting
    - Update server HomePage to import  and pass props to client/server sections
    - Prepare for swapping constants with PayloadCMS fetch

---
+++ src/constants/home/index.ts
@@ -0,0 +1,10 @@
+import { leftItems, rightItems } from './third_section';
+import { features } from './fourth_section';
+import { faq } from './faq';
+
+export const data = {
+  leftItems,
+  rightItems,
+  features,
+  faq,
+} as const;
