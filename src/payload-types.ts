c267153a75476662d9afe381f087eec9aa17c4a1
PyConqueror <wanaqim@gmail.com>
2025-08-17 17:41:22 +0800

    chore : update payload types and importmap

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -69,6 +69,9 @@ export interface Config {
   collections: {
     users: User;
     media: Media;
+    faq: Faq;
+    features: Feature;
+    blog: Blog;
     'payload-locked-documents': PayloadLockedDocument;
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
@@ -77,15 +80,28 @@ export interface Config {
   collectionsSelect: {
     users: UsersSelect<false> | UsersSelect<true>;
     media: MediaSelect<false> | MediaSelect<true>;
-    'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
-    'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
-    'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
+    faq: FaqSelect<false> | FaqSelect<true>;
+    features: FeaturesSelect<false> | FeaturesSelect<true>;
+    blog: BlogSelect<false> | BlogSelect<true>;
+    'payload-locked-documents':
+      | PayloadLockedDocumentsSelect<false>
+      | PayloadLockedDocumentsSelect<true>;
+    'payload-preferences':
+      | PayloadPreferencesSelect<false>
+      | PayloadPreferencesSelect<true>;
+    'payload-migrations':
+      | PayloadMigrationsSelect<false>
+      | PayloadMigrationsSelect<true>;
   };
   db: {
     defaultIDType: string;
   };
-  globals: {};
-  globalsSelect: {};
+  globals: {
+    homePage: HomePage;
+  };
+  globalsSelect: {
+    homePage: HomePageSelect<false> | HomePageSelect<true>;
+  };
   locale: null;
   user: User & {
     collection: 'users';
@@ -158,6 +174,74 @@ export interface Media {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "faq".
+ */
+export interface Faq {
+  id: string;
+  question: string;
+  answers: string;
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "features".
+ */
+export interface Feature {
+  id: string;
+  image: string | Media;
+  title: string;
+  open: string | Media;
+  desc: string;
+  support?: (string | null) | Media;
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "blog".
+ */
+export interface Blog {
+  id: string;
+  type:
+    | 'kesihatan'
+    | 'kelahiran'
+    | 'pendidikan'
+    | 'pekerjaan'
+    | 'keluarga'
+    | 'kediaman'
+    | 'pengangkutan'
+    | 'bantuan'
+    | 'perjalanan'
+    | 'persaraan'
+    | 'kematian'
+    | 'umum'
+    | 'hebahan';
+  title: string;
+  readtime: number;
+  datePublished: string;
+  picture?: (string | null) | Media;
+  caption?: string | null;
+  content: {
+    root: {
+      type: string;
+      children: {
+        type: string;
+        version: number;
+        [k: string]: unknown;
+      }[];
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+      indent: number;
+      version: number;
+    };
+    [k: string]: unknown;
+  };
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
@@ -170,6 +254,18 @@ export interface PayloadLockedDocument {
     | ({
         relationTo: 'media';
         value: string | Media;
+      } | null)
+    | ({
+        relationTo: 'faq';
+        value: string | Faq;
+      } | null)
+    | ({
+        relationTo: 'features';
+        value: string | Feature;
+      } | null)
+    | ({
+        relationTo: 'blog';
+        value: string | Blog;
       } | null);
   globalSlug?: string | null;
   user: {
@@ -255,6 +351,44 @@ export interface MediaSelect<T extends boolean = true> {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "faq_select".
+ */
+export interface FaqSelect<T extends boolean = true> {
+  question?: T;
+  answers?: T;
+  updatedAt?: T;
+  createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "features_select".
+ */
+export interface FeaturesSelect<T extends boolean = true> {
+  image?: T;
+  title?: T;
+  open?: T;
+  desc?: T;
+  support?: T;
+  updatedAt?: T;
+  createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "blog_select".
+ */
+export interface BlogSelect<T extends boolean = true> {
+  type?: T;
+  title?: T;
+  readtime?: T;
+  datePublished?: T;
+  picture?: T;
+  caption?: T;
+  content?: T;
+  updatedAt?: T;
+  createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-locked-documents_select".
  */
 export interface PayloadLockedDocumentsSelect<T extends boolean = true> {
@@ -287,12 +421,75 @@ export interface PayloadMigrationsSelect<T extends boolean = true> {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "homePage".
+ */
+export interface HomePage {
+  id: string;
+  leftItems: {
+    icon: string | Media;
+    title: string;
+    id?: string | null;
+  }[];
+  rightItems: {
+    icon: string | Media;
+    title: string;
+    id?: string | null;
+  }[];
+  features?:
+    | {
+        image: string | Media;
+        title: string;
+        open: string | Media;
+        desc: string;
+        support?: (string | null) | Media;
+        id?: string | null;
+      }[]
+    | null;
+  faq?: (string | Faq)[] | null;
+  updatedAt?: string | null;
+  createdAt?: string | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "homePage_select".
+ */
+export interface HomePageSelect<T extends boolean = true> {
+  leftItems?:
+    | T
+    | {
+        icon?: T;
+        title?: T;
+        id?: T;
+      };
+  rightItems?:
+    | T
+    | {
+        icon?: T;
+        title?: T;
+        id?: T;
+      };
+  features?:
+    | T
+    | {
+        image?: T;
+        title?: T;
+        open?: T;
+        desc?: T;
+        support?: T;
+        id?: T;
+      };
+  faq?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "auth".
  */
 export interface Auth {
   [k: string]: unknown;
 }
-
 
 declare module 'payload' {
   export interface GeneratedTypes extends Config {}
