cd14d6cbc901ec35effbc8d9ff28bd3f99d9ea4b
PyConqueror <wanaqim@gmail.com>
2025-08-19 13:08:30 +0800

    chore : fix merge bug error

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,9 +1,16 @@
 import { getMessages, type Locale } from '@/lib/i18n';
 import HomePage from '@/components/home/HomePage';
+import { getPayload } from 'payload';
+import config from '@/payload.config';
 
 export default async function HomeMainPage({ params }: { params: Promise<{ locale: Locale }> }) {
   const { locale } = await params;
   const messages = await getMessages(locale);
+  const payload = await getPayload({ config });
+  const data = await payload.findGlobal({
+    slug: 'homePage',
+    depth: 3,
+  });
 
-  return <HomePage messages={messages} />;
+  return <HomePage messages={messages} data={data} />;
 }
--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -4,13 +4,14 @@ import AboutSection from '@/components/home/AboutSection';
 import FeaturesTilesSection from '@/components/home/FeaturesTilesSection';
 import FeaturesCarousel from '@/components/home/FeaturesCarousel';
 import FAQSection from '@/components/home/FAQSection';
-import { data } from '@/constants/home';
+import { HomePage as HomePageType } from '@/payload-types';
 
 interface HomePageProps {
   messages: ReturnType<typeof import('@/lib/i18n').getMessages>;
+  data: HomePageType;
 }
 
-export default function HomePage({ messages }: HomePageProps) {
+export default function HomePage({ messages, data }: HomePageProps) {
   return (
     <div className="mx-auto px-[18px] sm:px-[18px] md:px-[24px] lg:px-[24px] xl:px-[24px] max-w-[1328px] py-16">
       {/* Hidden SPLaSK Contact Details tag for crawler detection */}
@@ -25,7 +26,7 @@ export default function HomePage({ messages }: HomePageProps) {
       {/* pass messages here for client component*/}
       <HeroSection messages={messages} />
       {/* pass messages here for client component*/}
-      <AboutSection messages={messages} />
+      <AboutSection title={data.aboutTitle} description={data.aboutDescription} />
       {/* payload data */}
       <FeaturesTilesSection leftItems={data.leftItems} rightItems={data.rightItems} />
       <FeaturesCarousel features={data.features} />
