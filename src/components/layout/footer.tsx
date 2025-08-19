a37fac3b914210cae38c77ff453ca71eed2d5097
PaanJoe <38974707+paanjoe@users.noreply.github.com>
2025-08-19 20:58:44 +0800

    feat: add social media links at footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Footer, SiteInfo, FooterSection, FooterLogo } from '@govtechmy/myds-react/footer';
-import { FacebookIcon, TwitterXIcon, InstagramIcon } from '@govtechmy/myds-react/icon';
+import { FacebookIcon, TwitterXIcon, InstagramIcon, TiktokIcon } from '@govtechmy/myds-react/icon';
 import Link from 'next/link';
 import { type Locale } from '@/lib/i18n';
 import Image from 'next/image';
@@ -78,22 +78,29 @@ export default function FooterComponent({}: FooterComponentProps) {
           </div>
           <div className="flex flex-row gap-3">
             <Link
-              href=""
+              href="https://www.facebook.com/mygovmy"
+              target="_blank"
               className=" bg-[#F1F5FF] p-2 rounded-full w-8 h-8 md:w-10 md:h-10 flex justify-center items-center hover:shadow-md hover:shadow-gray-300 hover:bg-gray-200"
             >
               <FacebookIcon scale={32} className="md:scale-40 text-[#2563EB]" />
             </Link>
             <Link
-              href=""
+              href="https://x.com/mygovmalaysia"
               className=" bg-[#F1F5FF] p-2 rounded-full w-8 h-8 md:w-10 md:h-10 flex justify-center items-center hover:shadow-md hover:shadow-gray-300 hover:bg-gray-200"
             >
               <TwitterXIcon scale={32} className="md:scale-40 text-[#2563EB]" />
             </Link>
             <Link
-              href=""
+              href="https://www.instagram.com/mygovmalaysia/"
               className=" bg-[#F1F5FF] p-2 rounded-full w-8 h-8 md:w-10 md:h-10 flex justify-center items-center hover:shadow-md hover:shadow-gray-300 hover:bg-gray-200"
             >
               <InstagramIcon scale={32} className="md:scale-40 text-[#2563EB]" />
+            </Link>
+            <Link
+              href="https://www.tiktok.com/@mygovmalaysia"
+              className=" bg-[#F1F5FF] p-2 rounded-full w-8 h-8 md:w-10 md:h-10 flex justify-center items-center hover:shadow-md hover:shadow-gray-300 hover:bg-gray-200"
+            >
+              <TiktokIcon scale={32} className="md:scale-40 text-[#2563EB]" />
             </Link>
           </div>
         </div>
