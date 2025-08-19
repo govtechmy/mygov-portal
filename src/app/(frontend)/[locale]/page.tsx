09bd45673cfe3b3454b9dc1ce9aa5f5ac187bf75
AMMAR HAIRI <145964801+ammarhairiHD@users.noreply.github.com>
2025-08-19 10:23:22 +0800

    Contain all homepage and using params for message instead

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,12 +1,7 @@
-import HeroSection from '@/components/home/HeroSection';
-import AboutSection from '@/components/home/AboutSection';
-import FeaturesTilesSection from '@/components/home/FeaturesTilesSection';
-import FeaturesCarousel from '@/components/home/FeaturesCarousel';
-import FAQSection from '@/components/home/FAQSection';
-import { data } from '@/constants/home';
 import { getMessages, type Locale } from '@/lib/i18n';
+import HomePage from '@/components/home/HomePage';
 
-export default async function HomePage({
+export default async function HomeMainPage({
   params,
 }: {
   params: Promise<{ locale: Locale }>;
@@ -14,26 +9,5 @@ export default async function HomePage({
   const { locale } = await params;
   const messages = await getMessages(locale);
 
-  return (
-    <>
-      {/* Hidden SPLaSK Contact Details tag for crawler detection */}
-      {/*<div
-      {...{ "splwpk-contact-details": "splwpk-contact-details" }}
-      {...{ "splwpk-contact-details-timestamp": timestamp }}
-      className="sr-only"
-      aria-hidden="true"
-    >
-      Contact Details Available
-    </div>*/}
-      {/* pass messages here for client component*/}
-      <HeroSection messages={messages} />
-      <AboutSection />
-      <FeaturesTilesSection
-        leftItems={data.leftItems}
-        rightItems={data.rightItems}
-      />
-      <FeaturesCarousel features={data.features} />
-      <FAQSection items={data.faq} />
-    </>
-  );
+  return <HomePage messages={messages} />;
 }
