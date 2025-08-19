5f4b1c29422c8ab6ae6da3e476dec9b3a3038f07
Zaid Aiman <zaidaiman@gmail.com>
2025-08-19 19:34:15 +0800

    minor fix

--- src/lib/contactValidation.ts
+++ src/lib/contactValidation.ts
@@ -3,12 +3,10 @@ import { z } from 'zod';
 export const contactSchema = z.object({
   category: z.string().min(1, 'Sila pilih kategori'), //yes
   name: z.string().min(2, 'Nama mesti lebih daripada 2 aksara'), //yes
-  ic: z
-    .string()
-    .regex(/^\d{6}-\d{2}-\d{4}$/, 'IC mesti dalam format 000000-00-0000'), //yes
+  ic: z.string().regex(/^\d{6}-\d{2}-\d{4}$/, 'IC mesti dalam format 000000-00-0000'), //yes
   address: z.string().min(5, 'Alamat tidak sah'),
   phoneCode: z.string().min(2),
-  phone: z.string().regex(/^[0-9]{7,10}$/, 'Nombor telefon tidak sah'),
+  phone: z.string().regex(/^[0-9]{7,12}$/, 'Nombor telefon tidak sah'),
   email: z.string().email('Alamat emel tidak sah'), //yes
   suggestion: z.string().min(5, 'Sila masukkan sekurang-kurangnya 5 aksara'),
   file: z
@@ -24,9 +22,7 @@ export const contactSchema = z.object({
     .refine(
       file => {
         if (!file) return true;
-        return ['application/pdf', 'image/jpeg', 'image/png'].includes(
-          file.type
-        );
+        return ['application/pdf', 'image/jpeg', 'image/png'].includes(file.type);
       },
       { message: 'Jenis fail tidak dibenarkan (hanya PDF/JPG/PNG)' }
     ),
